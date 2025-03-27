fac8b9ec86a704d6cb8dd58f811848484de77409
Aisha Mirza <84517249+ayshenm@users.noreply.github.com>
2025-03-27 14:02:26 +0400

    about compentent css fixed

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -6,9 +6,28 @@ import { BounceLoader, HashLoader, MoonLoader } from "react-spinners";
 export default function Loading() {
   const [loading, setLoading] = useState(true);
 
+  //   useEffect(() => {
+  //     const timer = setTimeout(() => setLoading(false), 2000); // 2 saniyə sonra gizlənsin
+  //     return () => clearTimeout(timer);
+  //   }, []);
+
+  //   useEffect(() => {
+  //     // Yükləmə müddətini simulyasiya edir (2 saniyə)
+  //     setTimeout(() => {
+  //       setLoading(false);
+  //       window.scrollTo(0, 0); // Yükləmə bitəndə yuxarı qaytarır
+  //     }, 2000);
+  //   }, []);
+
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 2000); // 2 saniyə sonra gizlənsin
-    return () => clearTimeout(timer);
+    // İlk açılışda səhifəni yuxarı qaytarır
+    // document.documentElement.scrollTop = 0;
+    // document.body.scrollTop = 0;
+
+    // Yükləmə müddəti (1.5 saniyə)
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
   }, []);
 
   if (!loading) return null; // Yüklənmə bitəndə heç nə göstərmə
--- src/sections/about/index.tsx
+++ src/sections/about/index.tsx
@@ -15,16 +15,15 @@ const About = () => {
 
       <div className="flex flex-col justify-center gap-y-8 lg:flex-row lg:gap-x-8">
         <MotionViewport variants={varSlide("left")} className="w-full lg:w-[500px]">
-          <div className="mt-5 md:mt-0">
-            <div className="relative rounded-full bg-gradient-to-bl w-48 h-48 md:w-64 md:h-64 lg:w-80 lg:h-80">
-              <div className="absolute inset-0 animate-profile_animate home_img">
+          <div className="flex justify-center items-center p-4">
+            <div className="relative rounded-full sm:w-48 sm:h-48 md:w-64 md:h-64 lg:w-80 lg:h-80 xl:w-96 xl:h-96 max-w-full">
+              <div className="!w-60 !h-40">
                 <Image
-                  sizes="auto"
                   src={AboutImg}
-                  alt=""
+                  alt="About Image"
                   layout="fill"
                   objectFit="cover"
-                  className="home_img "
+                  className="home_img"
                 />
               </div>
             </div>
@@ -32,20 +31,24 @@ const About = () => {
         </MotionViewport>
 
         <MotionViewport variants={varSlide("left")} className="w-full lg:w-3/5">
-          <div className="text-muted-foreground mb-4 text-right flex flex-col gap-y-4 p-4 lg:p-0">
-            <p className="text-xl text-purple">
+          <div className="bg-redtext-muted-foreground mb-4 text-right flex flex-col gap-y-4 p-4 lg:p-0">
+            <p className="text-xl text-purple  text-center md:!text-right">
               Welcome to my portfolio website. My name is Ayisha Mirza. I am a Frontend developer.
             </p>
 
             <p className="italic">
-              I am constantly improving myself in the field of frontend development. I have a solid
-              foundation in core web technologies such as HTML, CSS, and JavaScript, and I gain
-              experience working with modern frontend frameworks (for example, React or Next.js). I
-              actively worked on frontend development during my education and in my own projects. I
-              am interested in topics such as user interface design, UI/UX principles, and
-              responsive web design, and I constantly try to improve myself in these areas. I am
-              currently working as a <span className="text-purple">Junior Frontend Developer</span>{" "}
-              in a local startup.
+              I am a passionate frontend developer with a strong foundation in HTML, CSS, and
+              JavaScript, specializing in modern web technologies. I have hands-on experience
+              working with frameworks such as React, Next.js, and Vite, along with state management
+              tools like Redux. I excel in building scalable and visually appealing user interfaces
+              using Tailwind CSS, ShadCN UI, and Aceternity UI. Additionally, I have expertise in
+              Three.js, allowing me to create immersive 3D experiences on the web. I focus on UI/UX
+              principles, responsive design, and performance optimization to deliver seamless user
+              interactions. For authentication and security, I am proficient in NextAuth for
+              integrating secure and flexible authentication solutions in Next.js applications. My
+              continuous learning mindset drives me to stay updated with the latest trends and best
+              practices in frontend development, ensuring high-quality, cutting-edge solutions
+              <span className="text-purple"> Frontend Developer</span> in a local startup.
             </p>
           </div>
         </MotionViewport>
